63e336c3adc6c1708be795dd66edb9ad8d8d88ee
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-14 02:30:55 +0530

    home carousel with swiper js

---
+++ components/home_carousel.php
@@ -0,0 +1,22 @@
+<div class="container-fluid px-lg-4 mt-4">
+  <!-- Swiper -->
+  <div class="swiper swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="/images/carousel/carousel1.png" class="w-100 d-block"/>
+      </div>
+      <div class="swiper-slide">
+        <img src="/images/carousel/carousel2.png" class="w-100 d-block"/>
+      </div>
+      <div class="swiper-slide">
+        <img src="/images/carousel/carousel3.png" class="w-100 d-block"/>
+      </div>
+      <div class="swiper-slide">
+        <img src="/images/carousel/carousel4.png" class="w-100 d-block"/>
+      </div>
+      <div class="swiper-slide">
+        <img src="/images/carousel/carousel5.png" class="w-100 d-block"/>
+      </div>
+    </div>
+  </div>
+</div>
