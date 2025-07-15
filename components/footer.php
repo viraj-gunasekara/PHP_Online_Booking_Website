a5a510d7994d78bcbfc509a51386e9b7aaeaa0a0
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-15 23:59:50 +0530

    links make as reusable

--- components/footer.php
+++ components/footer.php
@@ -53,3 +53,11 @@
  <h6 class="text-center bg-dark text-white p-3 m-0">
   Designed and Developed by Viraj Gunasekara
  </h6>
+
+<!-- Common Links -->
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+<!-- Swiper Js -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<!-- custom JS -->
+<script src="/assets/scripts.js"></script>
