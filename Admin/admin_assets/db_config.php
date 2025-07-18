c2e4815af9ba5daa88ecbdc00d43ee13c8451ad5
viraj-gunasekara <vsgunasekara.dev@gmail.com>
2025-07-18 17:58:16 +0530

    create adminLogin direct to Dashboard, and functional logout btn

--- Admin/admin_assets/db_config.php
+++ Admin/admin_assets/db_config.php
@@ -12,4 +12,33 @@
     die("Cannot Connect to Database" .mysqli_connect_error());
   }
 
+  function filteration($data){
+    foreach($data as $key => $value){
+      $data[$key] = trim($value);
+      $data[$key] = stripslashes($value);
+      $data[$key] = htmlspecialchars($value);
+      $data[$key] = strip_tags($value);
+    }
+    return $data;
+  }
+
+  function select($sql,$values,$datatypes){
+    $con = $GLOBALS['con'];
+    if($stmt = mysqli_prepare($con,$sql))
+      {
+      mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+      if(mysqli_stmt_execute($stmt)){
+        $res = mysqli_stmt_get_result($stmt);
+        mysqli_stmt_close($stmt);
+        return $res;
+      }
+      else{
+        mysqli_stmt_close($stmt);
+        die("Query cannot be executed - Select");
+      }
+    }else{
+      die("Query cannot be prepared - Select");
+    }
+  }
+
 ?>
